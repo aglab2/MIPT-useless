950a063e589d90b558da3566f6e925a82d885855
aglab2 <aglab3@gmail.com>
2014-09-26 23:34:57 +0400

    Update

---
+++ test.c
@@ -0,0 +1,7 @@
+#define x(g)\
+#define y ## g z
+
+int main(){
+	x;
+	return 0;
+}
--- useless_beta.c
+++ useless_beta.c
@@ -17,41 +17,43 @@
 #define __STR(const_num) # const_num
 #define STR(const_num) __STR(const_num)
 
-#define PERROR(name, args...) \
-	do{printf(args); usage(name);}while(0)
-
 void usage(char * name){
-	assert (name);
-
-	printf("Usage: %s <filename>\n"
-		"<filename> type:\n"
-		"<time in seconds> <program to execute>\n",
-		name);
-
-	exit(EXIT_FAILURE);
+	printf("Usage: %s <filename>\n", name);
+	printf("<filename> type:\n");
+	printf("<time in seconds> <program to execute>\n");
 }
 
 int main(int argc, char *argv[], char *envp[]){
+char buf[3] = {};
+#define S(arg) # arg
+char mas[] = "abc"S(23)"def";
+printf ("%s\n", mas);
+scanf ("%2s", buf);
+pid_t pid;
 
-	if (argc != 2) PERROR(argv[0], "Wrong number of arguments!\n\n");
+	if (argc != 2){
+		usage(argv[0]); exit(EXIT_FAILURE);
+	}
 
 	FILE *fp;
 	fp = fopen(argv[1], "r");
 
-	if (fp == NULL) PERROR(argv[0], "Incorrect input file!\n\n");
+	if (fp == NULL){
+		usage(argv[0]); exit(EXIT_FAILURE);
+	}
 
 	char str[FILENAME_MAX] = {};
 	int t = 0;
-	int ret = 0;
 
-	while ((ret = fscanf(fp, "%d%"STR(FILENAME_MAX)"s", &t, str)) == 2){
+	while (fscanf(fp, "%d%"STR(FILENAME_MAX)"s", &t, str) == 2){
 		PRINTD("%s is executed with delay %d\n", str, t);
 
-		if (t < 0) PERROR(argv[0], "Incorrect time (%d) for process %s!\n\n", t, str);
-
-		pid_t pid = fork();
+		if (t < 0){
+			usage(argv[0]); exit(EXIT_FAILURE);
+		}
+		pid = fork();
 		if (pid == -1){
-			assert(!"Fork failure!");
+			assert(0 && "Fork failure!");
 		}else if (pid == 0){
 			PRINTD("%s is executed in child\n", str);
 			sleep(t);
@@ -62,8 +64,10 @@ int main(int argc, char *argv[], char *envp[]){
 	}
 
 	while (wait(NULL) != -1) {};
-	if (!feof(fp)) PERROR(argv[0], "File format is incorrect! %d parameters was read\n\n", ret);
+	if (!feof(fp)){
+		usage(argv[0]); exit(EXIT_FAILURE);
+	}
 
 	fclose(fp);
-	return EXIT_SUCCESS;
+	return 0;
 }
